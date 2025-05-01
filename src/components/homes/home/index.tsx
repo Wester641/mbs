4cf05e87f012a43326fe30ab36bcf3756d4e6c46
zafarzhon <zafarzhon77@gmail.com>
2025-05-01 17:20:29 +0600

    added google analitics

--- src/components/homes/home/index.tsx
+++ src/components/homes/home/index.tsx
@@ -1,14 +1,12 @@
 import CtaArea from "./CtaArea";
 import HeroArea from "./HeroArea";
 import Cta2Area from "./Cta2Area";
-// import BlogArea from "./BlogArea";
 import AboutArea from "./AboutArea";
-// import AwardsArea from "./AwardsArea";
-import ServiceArea from "./ServiceArea";
+
 import ProjectsArea from "./ProjectsArea";
 import WorkprocessArea from "./WorkprocessArea";
 import TestimonialArea from "./TestimonialArea";
-// import VideoAreaHomeOne from "./VideoAreaHomeOne";
+
 import HeaderOne from "../../../layouts/headers/HeaderOne";
 import FooterOne from "../../../layouts/footers/FooterOne";
 import BackToTop from "../../../common/BackToTop";
@@ -16,7 +14,6 @@ import OurTools from "./OurTools";
 import CategoryArea from "./CategoryArea";
 import ProcessArea from "../home-2/ProcessArea";
 import ServiceArea3 from "./ServiceArea3";
-// import PartnerArea from "./PartnerArea";
 
 const HomeOne = () => {
   return (
@@ -24,18 +21,13 @@ const HomeOne = () => {
       <HeaderOne />
       <HeroArea />
       <CtaArea />
-      <ServiceArea />
       <ServiceArea3 />
       <AboutArea />
-      {/* <PartnerArea /> */}
       <CategoryArea />
       <ProjectsArea />
       <ProcessArea />
       <WorkprocessArea />
-      {/* <VideoAreaHomeOne /> */}
-      {/* <AwardsArea /> */}
       <TestimonialArea />
-      {/* <BlogArea /> */}
       <OurTools />
       <Cta2Area />
       <FooterOne />
--- src/components/service/index.tsx
+++ src/components/service/index.tsx
@@ -1,4 +1,3 @@
-import ServiceArea from "./ServiceArea";
 import ServiceAbout from "./ServiceAbout";
 // import PriceArea from "../homes/home-2/PriceArea";
 import Cta2Area from "../homes/home/Cta2Area";
@@ -7,6 +6,7 @@ import Breacrumb from "../../common/Breacrumb";
 import FooterOne from "../../layouts/footers/FooterOne";
 import BackToTop from "../../common/BackToTop";
 import { useEffect } from "react";
+import ServiceArea3 from "../homes/home/ServiceArea3";
 
 const Service = () => {
   useEffect(() => {
@@ -17,7 +17,7 @@ const Service = () => {
     <>
       <HeaderOne />
       <Breacrumb title="Services" subtitle="Services" />
-      <ServiceArea />
+      <ServiceArea3 />
       <ServiceAbout />
       {/* <PriceArea style_2={true} /> */}
       <Cta2Area />
